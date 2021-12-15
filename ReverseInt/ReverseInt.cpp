d56dca5600b51963f2e444c1c96532fc4b1b24e0
Anton <itmmpol@yandex.ru>
2021-12-16 00:08:24 +0300

    Добавьте файлы проекта.

---
+++ ReverseInt/ReverseInt.cpp
@@ -0,0 +1,35 @@
+﻿#include <algorithm>>
+#include <iostream>
+#include <string>
+class Solution {
+public:
+    int reverse(int x) {
+        std::string res = std::to_string(x);
+        std::reverse(res.begin(),res.end());
+        auto start = res.begin();
+        while (start != res.end()) {
+            if (*start == 0) {
+                res.erase(*res.begin());
+            }
+            start++;
+        }
+        auto ans = std::stoll(res);
+        if (ans < pow(2, -31) || ans >(pow(2, 31) - 1)) {
+            return 0;
+        }
+        if (res[res.length() - 1] == '-') {
+            ans *= -1;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    Solution sol;
+
+    std::cout << sol.reverse(1534236469);
+}
+
+

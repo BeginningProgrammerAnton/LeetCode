b5b3160a7ca2df7499345dd7fb4cdfc3552ab05a
Anton <itmmpol@yandex.ru>
2021-12-21 17:10:14 +0300

    add Container with water task

---
+++ ContainerWithWater/ConteinerWithWater.cpp
@@ -0,0 +1,34 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        int my_size = height.size() - 1;
+        int maximum = 0;
+        auto start = height.begin();
+        auto end = height.end() - 1;
+        while (start != end) {
+            maximum = max(min(*start, *end) * my_size, maximum);
+            --my_size;
+            if (*start < *end) {
+                ++start;
+            }
+            else {
+                --end;
+            }
+        }
+        return maximum;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector <int> vec = {1,1};
+    std::cout << sol.maxArea(vec);
+}

5103117182812909b7ac697cf2186b10aa59af50
Anton <itmmpol@yandex.ru>
2021-12-25 02:38:59 +0300

    add 2 tasks 3Sum and 3SumClosest

---
+++ 3Sum/3Sum.cpp
@@ -0,0 +1,53 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        int cur;
+        vector<vector<int>> res;
+        vector <int> res_cur;
+        sort(nums.begin(), nums.end());
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            size_t first_search = i + 1;
+            size_t last_search = nums.size() - 1;
+            int right_num = 0 - nums[i];
+
+            while (last_search > first_search) {
+                int sum = nums[first_search] + nums[last_search];
+                if (sum < right_num) {
+                    ++first_search;
+                }
+                else if (sum > right_num) {
+                    --last_search;
+                }
+                else {
+                    res.push_back({ nums[i],nums[first_search],nums[last_search] });
+                    ++first_search;
+                    while (last_search > first_search && nums[first_search] == nums[first_search - 1]) {
+                        ++first_search;
+                    }
+                }
+            }
+         
+        }
+       return res;
+    }
+};
+
+
+
+int main()
+{
+    vector<int> num = {};
+    Solution sol;
+    auto a = sol.threeSum(num);
+}
---
+++ 3SumClosest/3SumCloset.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    //nums = [-1,2,1,-4], target = 1
+    //[-1,-1,1,1,3]target = -1
+    //
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int dist = 999;
+        int cur_dist = 999;
+        pair<int, int> min_dist = {999,999};
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            size_t start_search = i + 1;
+            size_t end_search = nums.size() - 1;
+            while (end_search > start_search) {
+                int sum = nums[start_search] + nums[end_search] + nums[i];
+                if (sum > target) {
+                    cur_dist = abs(target - sum);
+                    if (cur_dist < min_dist.first) {
+                        min_dist.first = cur_dist;
+                        min_dist.second = sum;
+                    }
+                    end_search--;
+                    continue;
+                }
+                if (sum < target) {
+                    cur_dist = abs(target - sum);
+                    if (cur_dist < min_dist.first) {
+                        min_dist.first = cur_dist;
+                        min_dist.second = sum;
+                    }
+                    ++start_search;
+                    continue;
+                }
+                return sum;
+            }
+        }
+        return min_dist.second;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> vec = { 0,0,0};
+    std::cout << sol.threeSumClosest(vec,1);
+}
+
